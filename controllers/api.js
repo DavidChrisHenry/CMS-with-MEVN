8e934c992e888be01e1e48882a1c5e4426a1662e
DavidChrisHenry <19521496@gm.uit.edu.vn>
2024-04-11 10:03:55 +0700

    first commit

---
+++ controllers/api.js
@@ -0,0 +1,82 @@
+/** @format */
+const Post = require("../models/posts");
+const fs = require("fs");
+
+module.exports = class API {
+  // fetch all posts
+  static async fetchAllPost(req, res) {
+    try {
+      const posts = await Post.find();
+      res.status(200).json(posts);
+    } catch (err) {
+      res.status(404).json({ message: err.message });
+    }
+  }
+
+  //fetch post by ID
+  static async fetchPostById(req, res) {
+    const id = req.params.id;
+    try {
+      const post = await Post.findById(id);
+      res.status(200).json(post);
+    } catch (err) {
+      res.status(404).json({ message: err.message });
+    }
+  }
+
+  //create a post
+  static async createPost(req, res) {
+    const post = req.body;
+    const imagename = req.file.filename;
+    post.image = imagename;
+    try {
+      await Post.create(post);
+      res.status(201).json({ message: "Post a created successfully!" });
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  }
+
+  //update a post
+  static async updatePost(req, res) {
+    const id = req.params.id;
+    let new_image = "";
+    if (req.file) {
+      new_image = req.file.filename;
+      try {
+        fs.unlinkSync("../uploads/" + req.body.old_image);
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      new_image = req.body.old_image;
+    }
+    const newPost = req.body;
+    newPost.image = new_image;
+
+    try {
+      await Post.findByIdAndUpdate(id, newPost);
+      res.status(200).json({ message: "Post Updated Successfully!" });
+    } catch (err) {
+      res.status(404).json({ message: err.message });
+    }
+  }
+
+  //delete a post
+  static async deletePost(req, res) {
+    const id = req.params.id;
+    try {
+      const result = await Post.findByIdAndDelete(id);
+      if (result.image != "") {
+        try {
+          fs.unlinkSync("./uploads/" + result.image);
+        } catch (err) {
+          console.log(err);
+        }
+      }
+      res.status(200).json({ message: "Post delete Successfully!" });
+    } catch (err) {
+      res.status(404).json({ message: err.message });
+    }
+  }
+};
